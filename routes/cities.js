2f0cb58fdd60719f14a47135a6f49ee57d8260df
Nicolás Proto <nicolasproto@gmail.com>
2019-06-06 23:16:52 -0300

    Query filters added

---
+++ routes/cities.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const City = require('../models/City.js')
+
+router.get('/', (req, res, next) => {
+	const query = req.query
+
+	City.find(query)
+	.then(cities => {
+		res.json({
+			confirmation: 'success',
+			data: cities
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const City = require('./models/City.js')
-const Country = require('./models/Country.js')
 
 // connect to our Mongo DB: 
 mongoose.connect('mongodb://localhost/world', { useNewUrlParser: true })
@@ -15,46 +13,13 @@ mongoose.connect('mongodb://localhost/world', { useNewUrlParser: true })
 
 const app = express()
 
-app.get('/', (req, res, next) => {
-	res.json({
-		confirmation: 'success',
-		data: 'This is the Mongo project!'
-	})
-})
+const cities = require('./routes/cities')
+app.use('/cities', cities)
 
-app.get('/cities', (req, res, next) => {
-	City.find(null)
-	.then(cities => {
-		res.json({
-			confirmation: 'success',
-			data: cities
-		})
-	})
-	.catch(err =>{
-		res.json({
-			confirmation: 'fail',
-			message: err.message
-		})
+const countries = require('./routes/countries')
+app.use('/countries', countries)
 
-	})
-})
 
-app.get('/countries', (req, res, next) => {
-	Country.find(null)
-	.then(countries => {
-		res.json({
-			confirmation: 'success',
-			data: countries
-		})
-	})
-	.catch(err =>{
-		res.json({
-			confirmation: 'fail',
-			message: err.message
-		})
-
-	})
-})
 
 app.listen(5000)
 console.log('App running http://localhost:5000')
