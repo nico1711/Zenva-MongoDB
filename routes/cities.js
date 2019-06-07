49a540af4bfb1958b046a3b23a9ce265fc8b4264
Nicolás Proto <nicolasproto@gmail.com>
2019-06-07 11:52:42 -0300

    Individual Queries

--- routes/cities.js
+++ routes/cities.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+// Needs the .. because it's on an a parent folder
 const City = require('../models/City.js')
 
+// Filters with the query given in the url
 router.get('/', (req, res, next) => {
 	const query = req.query
 
@@ -16,6 +18,25 @@ router.get('/', (req, res, next) => {
 		res.json({
 			confirmation: 'fail',
 			message: err.message
+		})
+	})
+
+})
+
+// Gets the Id and looks for an specific item
+router.get('/:id', (req, res, next) => {
+
+	City.findById(req.params.id)
+	.then(city => {
+		res.json({
+			confirmation: 'success',
+			data: city
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: 'City with Id ' + req.params.id + ' not found.'
 		})
 	})
 
--- routes/countries.js
+++ routes/countries.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+// Needs the .. because it's on an a parent folder
 const Country = require('../models/Country.js')
 
+// Filters with the query given in the url
 router.get('/', (req, res, next) => {
 	const query = req.query
 
@@ -16,6 +18,25 @@ router.get('/', (req, res, next) => {
 		res.json({
 			confirmation: 'fail',
 			message: err.message
+		})
+	})
+
+})
+
+// Gets the Id and looks for an specific item
+router.get('/:id', (req, res, next) => {
+
+	Country.findById(req.params.id)
+	.then(country => {
+		res.json({
+			confirmation: 'success',
+			data: country
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: 'Country with Id ' + req.params.id + ' not found.'
 		})
 	})
 
